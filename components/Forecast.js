8d2405bb0d7aea6d01e31b54224e360726598d43
6110110516 <60872701+6110110516@users.noreply.github.com>
2020-08-28 14:21:31 +0700

    Commit 3

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Forecast(props) {
     return (
+    <View style= {{
+        top: 37
+    }}>
+    <Text style = {styles.fonts}>{props.main}</Text>
+    <Text style = {styles.fonts}>{props.description}</Text>
     <View>
-    <Text>{props.main}</Text>
-    <Text>{props.description}</Text>
-    <View>
-    <Text>{props.temp}</Text>
-    <Text>°C</Text>
+    <Text style = {styles.fonts}>{props.temp} °C</Text>
+    {/* <Text>°C</Text> */}
     </View>
     </View>
     );
    }
+   const styles = StyleSheet.create({
+    
+    fonts:{ 
+        fontSize: 27,
+        height: 50
+        
+    },
+    
+})
+
--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Text, StyleSheet, ImageBackground, View } from 'react-native';
 import Forecast from './Forecast';
+import Constants from 'expo-constants';
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
@@ -11,17 +12,40 @@ export default function Weather(props) {
     return(
     <View>
         <ImageBackground source={require('../bg.jpeg')} style ={styles.backdrop}>
-            <Text>Zip Code</Text>
-            <Text>{props.zipCode}</Text>
-            <Forecast {...forecastInfo}/>
+            <View style = {styles.backfont}>
+                
+                    <Text style = {styles.fonts}>Zip Code is {props.zipCode}.</Text>
+                    <Forecast {...forecastInfo}/>
+                    </View>
         </ImageBackground>
         </View>    
     );
 }
 const styles = StyleSheet.create({
     backdrop:{
-        alignItems: 'center',
+        // flexDirection: 'column',
+        // justifyContent: 'flex-start',
+        
+        // alignItems: "center",
         width: '100%',
         height: '100%'
+    },
+    fonts:{ 
+        fontSize: 30,
+        top: 10,
+        bottom: 70
+    },
+    backfont:{
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        backgroundColor: 'skyblue',
+        position: 'absolute',
+        opacity: 0.7,
+        top: 0,
+        right: 0,
+        bottom: '65%',
+        left: 0,
+        
     }
 })
